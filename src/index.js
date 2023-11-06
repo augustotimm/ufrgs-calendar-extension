09cfb1b0fbc5623e172d161d989a80f887cf26a6
augusto <augusto.tes@gmail.com>
2023-11-06 15:41:39 -0300

    using matrix rule

--- src/index.js
+++ src/index.js
@@ -6,14 +6,16 @@ const content = [];
 
 const res = new Promise((resolve, reject) => {
     const rules = [
-        Rule.on(new RegExp("(.*\\d{1,2}\\/\\d{1,2}\\/\\d{2,4}.*)", "g"))
-            .extractRegexpValues()
-            .then((value) =>
-                content.push({ extractRegexpValues: value })
+        Rule.on(/^PRIMEIRO PERÍODO LETIVO DE 2022 \(2022\/1\)$/)
+            .parseTable(3)
+            .then((table) =>
+                content.push({
+                    "parseTable.renderMatrix": lib.parseTable.renderMatrix(
+                        table.matrix
+                    ),
+                    "parseTable.renderItems": lib.parseTable.renderItems(table.items),
+                })
             ),
-        Rule.on(new RegExp("(.*\\d{1,2}\\/\\d{1,2}\\/\\d{2,4}.*)", "g"))
-            .parseNextItemValue()
-            .then((value) => content.push({ parseNextItemValue: value })),
     ]
     const processItem = Rule.makeItemProcessor(rules);
     new PdfReader().parseFileItems("/home/augusto/repos/ufrgs-calendar-extension/files/portaria.pdf", (err, item) => {
