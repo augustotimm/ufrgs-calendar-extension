c1428c3bb9ded9bd39a7f6a0e129b082ab0ba394
augusto <augusto.tes@gmail.com>
2023-11-06 16:34:01 -0300

    content array being filled with organized data

--- src/index.js
+++ src/index.js
@@ -1,19 +1,69 @@
 import { PdfReader, Rule, parseTable} from "pdfreader";
 const content = [];
 
+const dateRegexp = new RegExp("(.*\\d{1,2}\\/\\d{1,2}\\/\\d{2,4}$)")
+const incompleteDateRegexp = new RegExp("(.*\\d{1,2}\\/\\d{1,2}\\/\\d{2,4} [a|à]s*)")
+const contentType = {
+    DATE_HEAD: "dateHead",
+    DATE_APPENDIX: "dateAppendix",
+    EVENT: "event"
+}
+
+const addParsedContent = function (text, type) {
+    switch (type) {
+        case contentType.DATE_APPENDIX:
+            content[content.length - 1].dateString += text;
+            return;
+        case contentType.DATE_HEAD:
+            content.push({dateString: text, eventString: ""});
+            return;
+        case contentType.EVENT:
+            content[content.length - 1].eventString += text;
+            return;
+        default:
+            console.log("add parsed content type error");
+            break;
+    }
+}
+
 
 
 const res = new Promise((resolve, reject) => {
     const rules = [
         Rule.on(/^PRIMEIRO PERÍODO LETIVO DE 2022 \(2022\/1\)$/)
             .parseTable(3)
-            .then((table) =>
-                content.push({
-                    "parseTable.renderMatrix": parseTable.renderMatrix(
-                        table.matrix
-                    ),
-                    "parseTable.renderItems": parseTable.renderItems(table.items),
-                })
+            .then((table) => {
+                    let dateAppendix = false;
+
+                    table.items.map(function (element, index) {
+                        const dateMatch = dateRegexp.test(element.text);
+
+                        if (incompleteDateRegexp.test(element.text) && !dateMatch) {
+                            dateAppendix = true;
+                            addParsedContent(element.text, contentType.DATE_HEAD);
+                            return;
+                        }
+
+                        if (dateMatch && dateAppendix) {
+                            dateAppendix = false;
+                            addParsedContent(element.text, contentType.DATE_APPENDIX);
+                            return;
+                        }
+                        if(dateMatch) {
+                            addParsedContent(element.text, contentType.DATE_HEAD);
+                            return;
+                        }
+
+                        content[content.length - 1].eventString += element.text;
+                        return;
+                    })
+                }
+                // content.push({
+                //     "parseTable.renderMatrix": parseTable.renderMatrix(
+                //         table.matrix
+                //     ),
+                //     "parseTable.renderItems": parseTable.renderItems(table.items),
+                // })
             ),
     ]
     const processItem = Rule.makeItemProcessor(rules);
