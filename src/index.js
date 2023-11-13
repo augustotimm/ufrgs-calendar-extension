336a59d9ae40459f8f28a87dc09d42bb067cfcc9
augusto <augusto.tes@gmail.com>
2023-11-13 17:19:56 -0300

    parsed all date strings to date objects

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import { PdfReader, Rule, parseTable} from "pdfreader";
+import { PdfReader, Rule} from "pdfreader";
 const content = [];
 
-const dateRegexp = new RegExp("(.*\\d{1,2}\\/\\d{1,2}\\/\\d{2,4}$)")
-const incompleteDateRegexp = new RegExp("(.*\\d{1,2}\\/\\d{1,2}\\/\\d{2,4} [a|à]s*)")
-const singleDateRegexp = new RegExp("\\d{1,2}\\/\\d{1,2}\\/\\d{2,4}", "g")
+const dateRegexp = new RegExp("(.*\\d{1,2}\\/\\d{1,2}\\/\\d{2,4}$)");
+const incompleteDateRegexp = new RegExp("(.*\\d{1,2}\\/\\d{1,2}\\/\\d{2,4} [a|à]s*)");
+const singleDateRegexp = new RegExp("(\\d{1,2}\\/\\d{1,2}\\/\\d{2,4})", "g");
+
+const containHoursRegexp = new RegExp("(\\d{1,2}h\\d{0,2}).*(\\d{1,2}h\\d{0,2})");
+
+// const dateRangeRegexp = new RegExp("\\d{1,2}\\/\\d{1,2}\\/\\d{2,4} (a|até) \\d{1,2}\\/\\d{1,2}\\/\\d{2,4}", "i");
+const hourStarRangeRegexp = new RegExp("das (\\d{1,2}h\\d{0,2}) de \\d{1,2}\\/\\d{1,2}\\/\\d{2,4}", "i");
+const hourEndRangeRegexp = new RegExp("às(\\d{1,2}h\\d{0,2}) de \\d{1,2}\\/\\d{1,2}\\/\\d{2,4}", "i");
+const hourRangeSingleDateRegexp = new RegExp("das (\\d{1,2}h\\d{0,2}) às\\s*(\\d{1,2}h\\d{0,2})", "i");
+
 
 const contentType = {
     DATE_HEAD: "dateHead",
@@ -28,16 +36,54 @@ const addParsedContent = function (text, type) {
     }
 }
 
-const parseDateString = function(event) {
+const parseHourString = function(hourString) {
+    const times = hourString.split('h');
+    return {hour: parseInt(times[0]), minutes: parseInt(times[1]) || 0}
+}
+
+const parseDateString = function({dateString}) {
     let date;
     const dates = [];
+    const hours = [];
+    let iterator = 0;
+    let singleDate = false;
+    const containHour = containHoursRegexp.test(dateString);
 
-    while ((date = singleDateRegexp.exec(event.dateString)) !== null) {
-        console.log(`Found ${date[0]}. Next starts at ${singleDateRegexp.lastIndex}.`);
+    if(containHour) {
+        let startHourObject;
+        let endHourObject
+        if((date = hourRangeSingleDateRegexp.exec(dateString)) !== null) {
+            startHourObject = parseHourString(date[1]);
+            endHourObject = parseHourString(date[2]);
+            singleDate = true;
+
+        } else {
+            const startHourString = hourStarRangeRegexp.exec(dateString);
+            const endHourString = hourEndRangeRegexp.exec(dateString);
+
+            startHourObject = parseHourString(startHourString[1])
+            endHourObject = parseHourString(endHourString[1])
+        }
+        hours.push(startHourObject);
+        hours.push(endHourObject);
+    }
+    while ((date = singleDateRegexp.exec(dateString)) !== null) {
         const dateParts = date[0].split('/')
-        dates.push(new Date(parseInt(dateParts[2]), parseInt(dateParts[1]) - 1, parseInt(dateParts[0])));
-        // Expected output: "Found foo. Next starts at 9."
-        // Expected output: "Found foo. Next starts at 19."
+        if(containHour) {
+            dates.push(new Date(parseInt(dateParts[2]), parseInt(dateParts[1]) - 1, parseInt(dateParts[0]), hours[iterator].hour, hours[iterator].minutes));
+
+        } else {
+            dates.push(new Date(parseInt(dateParts[2]), parseInt(dateParts[1]) - 1, parseInt(dateParts[0])));
+        }
+
+        iterator ++;
+        if(singleDate) {
+            dates.push(new Date(parseInt(dateParts[2]), parseInt(dateParts[1]) - 1, parseInt(dateParts[0]), hours[iterator].hour, hours[iterator].minutes));
+
+        }
+    }
+    if( dates.length > 2) {
+        console.log(dateString);
     }
     return dates;
 }
@@ -50,7 +96,7 @@ const res = new Promise((resolve, reject) => {
             .then((table) => {
                     let dateAppendix = false;
 
-                    table.items.map(function (element, index) {
+                    table.items.map(function (element) {
                         const dateMatch = dateRegexp.test(element.text);
 
                         if (incompleteDateRegexp.test(element.text) && !dateMatch) {
@@ -90,7 +136,6 @@ const res = new Promise((resolve, reject) => {
         }
     });
 })
-await res
-parseDateString(content[0]);
-
-console.log(content)
+await res;
+const parsedDates = content.map(parseDateString)
+console.log(parsedDates)
