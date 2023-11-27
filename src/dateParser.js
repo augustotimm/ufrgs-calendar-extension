c0090c6de75d62b180f4299393015d4245f3d75d
augusto <augusto.tes@gmail.com>
2023-11-27 17:00:14 -0300

    separated parsing methods to different module

---
+++ src/dateParser.js
@@ -0,0 +1,62 @@
+const singleDateRegexp = new RegExp("(\\d{1,2}\\/\\d{1,2}\\/\\d{2,4})", "g");
+
+const containHoursRegexp = new RegExp("(\\d{1,2}h\\d{0,2}).*(\\d{1,2}h\\d{0,2})");
+
+// const dateRangeRegexp = new RegExp("\\d{1,2}\\/\\d{1,2}\\/\\d{2,4} (a|até) \\d{1,2}\\/\\d{1,2}\\/\\d{2,4}", "i");
+const hourStarRangeRegexp = new RegExp("das (\\d{1,2}h\\d{0,2}) de \\d{1,2}\\/\\d{1,2}\\/\\d{2,4}", "i");
+const hourEndRangeRegexp = new RegExp("às(\\d{1,2}h\\d{0,2}) de \\d{1,2}\\/\\d{1,2}\\/\\d{2,4}", "i");
+const hourRangeSingleDateRegexp = new RegExp("das (\\d{1,2}h\\d{0,2}) às\\s*(\\d{1,2}h\\d{0,2})", "i");
+
+
+const parseHourString = function(hourString) {
+    const times = hourString.split('h');
+    return {hour: parseInt(times[0]), minutes: parseInt(times[1]) || 0}
+}
+
+export const parseDateString = function({dateString}) {
+    let date;
+    const dates = [];
+    const hours = [];
+    let iterator = 0;
+    let singleDate = false;
+    const containHour = containHoursRegexp.test(dateString);
+
+    if(containHour) {
+        let startHourObject;
+        let endHourObject
+        if((date = hourRangeSingleDateRegexp.exec(dateString)) !== null) {
+            startHourObject = parseHourString(date[1]);
+            endHourObject = parseHourString(date[2]);
+            singleDate = true;
+
+        } else {
+            const startHourString = hourStarRangeRegexp.exec(dateString);
+            const endHourString = hourEndRangeRegexp.exec(dateString);
+
+            startHourObject = parseHourString(startHourString[1])
+            endHourObject = parseHourString(endHourString[1])
+        }
+        hours.push(startHourObject);
+        hours.push(endHourObject);
+    }
+    while ((date = singleDateRegexp.exec(dateString)) !== null) {
+        const dateParts = date[0].split('/')
+        if(containHour) {
+            dates.push(new Date(parseInt(dateParts[2]), parseInt(dateParts[1]) - 1, parseInt(dateParts[0]), hours[iterator].hour, hours[iterator].minutes));
+
+        } else {
+            dates.push(new Date(parseInt(dateParts[2]), parseInt(dateParts[1]) - 1, parseInt(dateParts[0])));
+        }
+
+        iterator ++;
+        if(singleDate) {
+            dates.push(new Date(parseInt(dateParts[2]), parseInt(dateParts[1]) - 1, parseInt(dateParts[0]), hours[iterator].hour, hours[iterator].minutes));
+
+        }
+    }
+    if( dates.length > 2) {
+        console.log(dateString);
+    }
+    return dates;
+}
+
