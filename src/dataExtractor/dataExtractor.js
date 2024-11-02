d4e9b5d4fcf121ec8bf74bf683a806cb6f7793b3
Timm <augusto.timm.do.espirito.santo@sap.com>
2024-11-02 08:45:46 -0300

    separated data extraction to its own module

---
+++ src/dataExtractor/dataExtractor.js
@@ -0,0 +1,57 @@
+import {PdfReader, TableParser} from "pdfreader";
+import {StateMachine} from "./stateMachine.js";
+
+const tableStarterPhrase = "Primeiro Período Letivo de";
+
+const ignoreString = "Segundo Período Letivo de 2024";
+
+const columnSeparator = (item) => parseFloat(item.x) >= 9;
+
+const mergeCells = (cells) => (cells || []).map((cell) => cell.text).join("");
+
+
+
+async function extractMatrixFromPDF(filepath) {
+    const extractedContent = [];
+    var table = new TableParser();
+    const pdfReader = new PdfReader();
+    return new Promise((resolve, reject) => {
+        pdfReader.parseFileItems(filepath, function (err, item) {
+            if (err) {
+                console.error(err);
+                reject(err);
+            }
+            if(!item) {
+                resolve(extractedContent);
+                return;
+            }
+
+            if (item.page) {
+                // end of file, or page
+                const formattedMatrix = table.getMatrix().map((matrix) => {
+                    return matrix.map(mergeCells);
+                });
+                extractedContent.push(...formattedMatrix);
+                item?.page && console.log("PAGE:", item.page);
+                table = new TableParser(); // new/clear table for next page
+            } else if (item.text) {
+                // accumulate text items into rows object, per line
+                table.processItem(item, columnSeparator(item));
+            }
+
+        });
+    });
+}
+
+/*
+    {
+        dateString: string,
+        eventString: string, // event name
+    }
+*/
+export async function extractDataFromPDF(filepath, firstWord = tableStarterPhrase, lastWord, separator = ignoreString){
+    const extractedContent = await extractMatrixFromPDF(filepath);
+    const stateMachine = new StateMachine();
+    return stateMachine.run(extractedContent, firstWord, lastWord, separator);
+
+}
